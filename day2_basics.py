9068d186413b1a75604a0bc509ea62ad7952f000
supriya <supriya10@gmail.com>
2026-01-23 09:23:34 +0530

    Day 2 python micro practice

---
+++ day2_basics.py
@@ -0,0 +1,11 @@
+Name = "Supriya"
+studied_hours=3
+
+print("Name:",Name)
+
+if studied_hours>=2:
+   print("well done ! you studied enough today")
+else:
+   print("you need to study a little more" )
+   
+   
