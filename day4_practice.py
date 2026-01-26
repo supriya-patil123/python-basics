3c4fd2c7b37de5487a3e58003f29d21f2dccd3de
supriya <supriya10@gmail.com>
2026-01-26 06:03:06 +0530

    day 4 practice python if-else and loop

---
+++ day4_practice.py
@@ -0,0 +1,12 @@
+print("Day 4 practice")
+
+number=int(input("Enter number:"))
+
+if number % 2 == 0:
+    print("number is even ")
+else:
+    print("number is odd")
+
+for i in range(1,6):
+    print("square of ",i,"is",i*i)
+    
