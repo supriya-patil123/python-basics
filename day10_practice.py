fd5ef886d6797d22fdb1d78322e2ba51f6c8fb5d
supriya <supriya10@gmail.com>
2026-02-01 08:54:57 +0530

    Day 10 :Light string practice(exam mode)

---
+++ day10_practice.py
@@ -0,0 +1,10 @@
+print("Day 10 python practice")
+
+#prints the number of strings
+string = input("Enter the string :")
+print(len(string))
+
+#prints the reversed string
+reversed_string = string[::-1]
+print(reversed_string)
+
