66aea3b9bb07fd68ffb50c0c3008252d77b30efb
supriya <supriya10@gmail.com>
2026-02-02 08:49:53 +0530

    Daily python touch :basic conditional practice

---
+++ day11_ultralight.py
@@ -0,0 +1,15 @@
+print("day 11 light practice due to exams")
+#take the string and print upper and lower case
+string = input("Enter the string:")
+
+print(string.upper())
+print(string.lower())
+
+#take 2 numbers and prints the greater
+num1 = int(input("Enter num1:"))
+num2 = int(input("Enter num2:"))
+
+if num1>num2:
+    print(num1 ,"is greater")
+else:
+    print(num2,"is greater")
