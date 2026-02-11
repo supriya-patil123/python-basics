8cad26cc7d66b9152c6e63a498349f7393d50dd8
supriya <supriya10@gmail.com>
2026-02-11 08:12:17 +0530

    Daily python touch:check starting vowel

--- daily_python_touch.py
+++ daily_python_touch.py
@@ -5,3 +5,11 @@ last_char = string[-1]
 
 print("first char is:",first_char)
 print("last char is:",last_char)
+
+text = input("Enter the string:")
+
+if len(text) > 0 and text[0].lower() in "aeiou":
+    print("starts with vowel")
+else:
+    print("does'nt starts with vowel")
+
