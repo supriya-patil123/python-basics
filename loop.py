03c08bb6661a2747276fdf814dedd55d8383d4ac
supriya <supriya10@gmail.com>
2026-03-05 09:30:55 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,19 @@
+#print 1 to 100
+
+for i in range(1,14):
+    print(i)
+
+
+#sum of n numbers
+sum =0
+for n in range(0,4):
+    sum= sum + n
+    
+print(sum)
+
+#multiplication table
+i=0
+while(i<=10):
+    print("5 *",i,"=",(5*i))
+    i = i+1
+
